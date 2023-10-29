473ad5cae68fe22c947e7eef4bd768b657dc3e5e
Riccardo Gagliarducci <riccardo@brixel.it>
2023-10-29 23:33:19 +0100

    Initial work.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # brixel-colorful-admin
 Changes the admin backend background color based on the siteurl, just for admin.
+
+## Motivation
+Did you ever happen to make a change on a wordpress in production thinking you were in localhost (opened in the side tab)?
+Have you ever happened to make an edit locally thinking it was in production and spend a lot of time debugging why the edit you just made doesn't work?
+
+This plugin does not solve all the problems but it alleviates them: each instance of wordpress will have the admin page background of a different color, stable over time.
+So you will have, for example, purple background and production will have green background. If your colleague will have his own instance on his pc it is possible that it will be light blue. This obviously works if siteurl is different from instance to instance...
+
---
+++ brixel-colorful-admin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name:	Brixel Colorful Admin
+ * Description:	Changes the admin backend background color based on the siteurl, just for admin. A test using AI.
+ * Version:		1.0
+ * Author: 		Brixel
+ * Author URI:	https://www.brixel.it
+ * License:		GPL v2 or later
+ * License URI:	https://www.gnu.org/licenses/gpl-2.0.html 
+ */
+
+
+// Define a function to generate a color based on the siteurl.
+function generate_background_color() {
+    $siteurl = get_option('siteurl');
+    $hash = md5($siteurl);
+    $color = '#' . substr($hash, 0, 6); // Use the first 6 characters of the MD5 hash as the color.
+    return $color;
+}
+
+// Define a function to add custom CSS to change the admin background color.
+function change_admin_background_color() {
+	if (current_user_can('administrator')) {
+
+		$color = generate_background_color();
+		echo '<style>
+		    body.wp-admin {
+		        background-color: ' . $color . ';
+		    }
+		</style>';
+    }
+}
+
+// Hook the custom CSS function to the admin_head action.
+add_action('admin_head', 'change_admin_background_color');
